d420c5bd12d6c440fd80c503c515834bcd3ab985
PT7994 <titus_peter@yahoo.com>
2025-12-11 18:00:50 -0500

    Update s7-search-products.js

--- s7-search-products.js
+++ s7-search-products.js
@@ -5,7 +5,8 @@ import fetch from "node-fetch";
 export default async function handler(req, res) {
 
   // ----- CORS FIX -----
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin: https://store7994.com
+);
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   if (req.method === "OPTIONS") return res.status(200).end();
