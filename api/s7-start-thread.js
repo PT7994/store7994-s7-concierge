93500889db3246ec7c15e059ca16bcb6d076f091
PT7994 <titus_peter@yahoo.com>
2025-12-10 21:56:22 -0500

    Switch API key to new environment variable CHATGPT_REAL_KEY

--- api/s7-start-thread.js
+++ api/s7-start-thread.js
@@ -1,10 +1,12 @@
-// force redeploy
+// fresh redeploy 2
 
 import OpenAI from "openai";
 
 export default async function handler(req, res) {
   try {
-    const client = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+    
+    const client = new OpenAI({ apiKey: process.env.CHATGPT_REAL_KEY });
+
 
     // Create a new thread
     const thread = await client.beta.threads.create();
