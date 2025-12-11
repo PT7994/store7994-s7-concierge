a5b30b9061250f5cee4a5d71eecf12b5d72e002f
PT7994 <titus_peter@yahoo.com>
2025-12-10 19:43:26 -0500

    Create s7-start-thread.js

---
+++ api/s7-start-thread.js
@@ -0,0 +1,18 @@
+import OpenAI from "openai";
+
+export default async function handler(req, res) {
+  try {
+    const client = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+
+    // Create a new thread
+    const thread = await client.beta.threads.create();
+
+    res.status(200).json({
+      threadId: thread.id
+    });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Unable to create thread" });
+  }
+}
